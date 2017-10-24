242d553d77fa8c4b10b0e536b7f461d9d156af22
Eri <erika@CableLink-168-90-71-198.host.InterCable.net>
2017-10-24 15:20:44 -0500

    ejercicio comparacion

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
 var comparación = function (array1, array2){
   document.write("funcióndeclarada es una prueba")
+  if (array1.length !== array2.length) {
+    retur false;
+  }else{
+    for (var i = 0; i< array1.length; i++) {
+      console.log(array1[i], array2[i])
+      if array1[i] !== array2[i]{
+        //document.write ("los arreglos son diferentes")
+        return false;
+      }
+    }
+    document.write("los arreglos son iguales")
+    return true;
+  }
 }
+
+var arreglo1 = [1,2,3,4,5,6,7,8,9,0]
+var arreglo2 = [1,2,3,4,5,6,7,8,9,0]
--- readme.md
+++ readme.md
@@ -1,6 +1,6 @@
  Comparison
 1. Crear una función que reciba 2 arreglos como parámetro
-2. coprobar si la longitud de los arreglos es difeente
+2. Comprobar si la longitud de los arreglos es difeente
    2.1 si los arreglos no son iguales devolver falso
    2.2 si tienen la misma longitud continuar el proceso.
 3. Comprobar uno a uno los valores del arreglo
@@ -8,3 +8,5 @@
    3.2 Si son iguales sigue comparando uno a uno
 4. Regresamos un true
 5. Fin :)
+
+![Esta es la imagen del diagrama] (Diagrama.jpg)
